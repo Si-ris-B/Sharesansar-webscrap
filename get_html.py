4ab3766c016f4c9e817260643f8e55f1975e6857
Si-ris-B <51501708+Si-ris-B@users.noreply.github.com>
2021-11-29 22:12:19 +0545

    Click next page

--- get_html.py
+++ get_html.py
@@ -8,9 +8,22 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
 
-def click_and_download(number):
-    button = browser.find_element(By.XPATH,'//*[@id="btn_cpricehistory"]')
+def click_and_download(number,delay):
+
+    if number < 5:
+        number = 3
+
+    xpath1 = f'/html/body/div[2]/div/section[2]/div[3]/div/div/div/div[2]/div/div[1]/div[2]/div/div[8]/div/div/div[5]/span/a[{number}]'
+    button = browser.find_element(By.XPATH,xpath1)
+    
     button.click()
+
+    WebDriverWait(browser, delay).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, selector))
+    )
+    html = browser.page_source
+
+
 
 html = None
 url = 'https://www.sharesansar.com/company/adbl'
@@ -46,11 +59,6 @@ finally:
 
 if html:
     soup = BeautifulSoup(html, 'lxml')
-    # raw_data = soup.select_one(selector).text
-    # data = json.loads(raw_data)
-
-    # import pprint
-    # pprint.pprint(data)
-    # soup = BeautifulSoup(html)
+    
     with open("test1.txt", "w") as f:
         f.write(soup.prettify())
